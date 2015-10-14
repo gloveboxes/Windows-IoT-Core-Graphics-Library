695436b797f7472b351d18dc1b8c0f4496d09566
Dave Glover <dglover@microsoft.com>
2015-10-14 21:39:13 +1100

    doco

--- HelloWorld/StartupTask.cs
+++ HelloWorld/StartupTask.cs
@@ -21,9 +21,6 @@ namespace HelloWorld
             MAX7219 driver = new MAX7219(4, MAX7219.Rotate.D90, MAX7219.ChipSelect.CE0);  // 4 panels, rotate 90 degrees, SPI CE0
             LED8x8Matrix matrix = new LED8x8Matrix(driver);     // pass the driver to the LED8x8Matrix Graphics Library
 
-
-            var t = new MAX7219(1, MAX7219.Rotate.None, MAX7219.ChipSelect.CE0,
-
             while (true) {
                 matrix.ScrollStringInFromRight("Hello World 2015", 100);
             }
